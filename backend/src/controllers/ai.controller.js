e234b39291291984e955b3eda500bfc66b585baf
Swapnaneel Ray <neel26ray@gmail.com>
2025-02-13 01:46:26 +0530

    Backend done

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -1,14 +1,15 @@
 const aiService = require("../services/ai.services");
 
-const getResponse = async (req, res) => {
-  const prompt = req.query.prompt;
+const getReview = async (req, res) => {
+  const lang = req.body.lang;
+  const prompt = req.body.prompt;
   if (!prompt) {
     return res.status(400).json({ error: "Prompt is required" });
   }
-  const response = await aiService.generateContent(prompt);
+  const response = await aiService.generateContent(lang + prompt);
   console.log(response);
 
   res.send(response);
 };
 
-module.exports = { getResponse };
+module.exports = { getReview };
