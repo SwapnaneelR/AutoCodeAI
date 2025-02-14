38a1e12f7b5183f739faceb8bc132052a1dd4d05
Swapnaneel Ray <neel26ray@gmail.com>
2025-02-15 01:14:34 +0530

    WOrking

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,6 @@ const app = require("./src/app");
 const path = require("path");
 
 const port = process.env.PORT || 5000;
-const dirname = path.resolve();
-app.use(express.static(path.join(dirname, "/frontend/dist")));
-app.get("*", (_, res) => {
-  res.sendFile(path.resolve(dirname, "frontend", "dist", "index.html"));
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- backend/src/app.js
+++ backend/src/app.js
@@ -4,7 +4,9 @@ const cors = require("cors");
 const aiRoutes = require("./routes/ai.routes");
 
 const app = express();
-
+app.get("/", (req, res) => {
+  res.send("Works");
+});
 app.use(express.json());
 app.use(cors());
 app.use("/ai", aiRoutes);
