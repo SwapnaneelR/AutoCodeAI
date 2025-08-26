3c31381650f7043c74dbb86ed1a577a5ffd3e9fd
unknown <neel26ray@gmail.com>
2025-08-27 00:33:25 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,19 @@ const cors = require("cors");
 const aiRoutes = require("./routes/ai.routes");
 
 const app = express();
+ 
+app.use(cors({
+  origin: "*",  
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Works");
 });
-app.use(express.json());
-app.use(cors());
+
 app.use("/ai", aiRoutes);
 
 module.exports = app;
